358abfb9daca2eb9741211bbc489c5f4f324a605
Sami Nurmivaara <sami@nurmivaara.fi>
2020-11-07 15:58:12 +0200

    Added better handling for post requests with strict typing and uuid generation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,11 @@ import {
   getInsightsForProduct,
   getProducts,
 } from './service/data-lake'
-import { postEvent, getEventHubProperties } from './service/event-hub'
+import {
+  getEventHubProperties,
+  postPurchase,
+  postReview,
+} from './service/event-hub'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -25,7 +29,9 @@ connect()
 app.get('/api/insights/:productId', (req, res) => {
   if (!req.params.productId)
     return res.status(400).json({ error: 'Missing productId path param' })
-  getInsightsForProduct(Number(req.params.productId)).then(insights => res.json(insights))
+  getInsightsForProduct(Number(req.params.productId)).then(insights =>
+    res.json(insights)
+  )
 })
 
 app.get('/api/products', (req, res) =>
@@ -36,9 +42,15 @@ app.get('/api/eventhub-info', (req, res) =>
   getEventHubProperties().then(data => res.json(data))
 )
 
-app.post('/api/post', (req, res) =>
-  postEvent(req.body)
-    .then(() => res.json('post successfull'))
+app.post('/api/purchase', (req, res) =>
+  postPurchase(req.body)
+    .then(() => res.json('Purchase posted successfully'))
+    .catch(err => console.error(err))
+)
+
+app.post('/api/review', (req, res) =>
+  postReview(req.body)
+    .then(() => res.json('Review posted succesfully'))
     .catch(err => console.error(err))
 )
 
--- backend/src/service/event-hub.ts
+++ backend/src/service/event-hub.ts
@@ -1,39 +1,51 @@
 import { EventHubProducerClient } from '@azure/event-hubs'
+import { v4 as uuidv4 } from 'uuid'
 
 const producerClient = new EventHubProducerClient(
   process.env.EVENTHUB_CONNECTION
 )
 
-// type Review = {
-//   review_uniqueID: string
-//   review: number
-//   review_ts: Date
-// }
+type Review = {
+  review_uniqueID: string
+  review: number
+  review_ts: Date
+}
 
-// type Purchase = {
-//   HEADER_ID: string
-//   ITEM_ID: string
-//   HEADER_BOOKINGDATE: Date
-//   HEADER_JOURNALTIME: Date
-//   HEADER_TERMINAL: string
-//   HEADER_CASHIER: string
-//   HEADER_TOTAL: number
-//   ITEM_CODE: number
-//   ITEM_DESCRIPTION: string
-//   ITEM_AMT: number
-//   ITEM_QTY: number
-//   ITEM_UNIT: string
-//   ITEM_NORMAL_PRICE: number
-//   REVIEW_UNQIUE_KEY: string
-// }
+type Purchase = {
+  HEADER_ID: string
+  ITEM_ID: string
+  HEADER_BOOKINGDATE: Date
+  HEADER_JOURNALTIME: Date
+  HEADER_TERMINAL: string
+  HEADER_CASHIER: string
+  HEADER_TOTAL: number
+  ITEM_CODE: number
+  ITEM_DESCRIPTION: string
+  ITEM_AMT: number
+  ITEM_QTY: number
+  ITEM_UNIT: string
+  ITEM_NORMAL_PRICE: number
+  REVIEW_UNQIUE_KEY: string
+}
 
 type Event = {
-  event_data: any
+  event_data: Purchase | Review
   event_domain: string
   event_id: string
   event_source: string
   event_ts: Date
   event_type: string
+}
+
+const generateEvent = (eventType: string, eventData: Purchase | Review) => {
+  return {
+    event_data: eventData,
+    event_domain: 'team_09',
+    event_id: uuidv4(),
+    event_source: 'tarant-coffee',
+    event_ts: new Date(),
+    event_type: eventType,
+  }
 }
 
 export const getEventHubProperties = async () =>
@@ -43,4 +55,22 @@ export const postEvent = async (event: Event) => {
   const eventDataBatch = await producerClient.createBatch()
   await eventDataBatch.tryAdd({ body: event })
   await producerClient.sendBatch(eventDataBatch)
+}
+
+export const postPurchase = async (body: Purchase) => {
+  const purchaseEvent = generateEvent(
+    `cafe_pos_data_v${process.env.TABLEVERSION}`,
+    body
+  )
+  console.log('purchaseEvent', purchaseEvent)
+  await postEvent(purchaseEvent)
+}
+
+export const postReview = async (body: Review) => {
+  const reviewEvent = generateEvent(
+    `review_data_v${process.env.TABLEVERSION}`,
+    body
+  )
+  console.log('reviewEvent', reviewEvent)
+  await postEvent(reviewEvent)
 }

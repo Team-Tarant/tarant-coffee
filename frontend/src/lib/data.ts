399c66670471dc2663581a6d95bbe82c0c78e124
myzozoz <markusp_holopainen@yahoo.com>
2020-11-07 19:49:06 +0200

    Skiljade url

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -1,15 +1,17 @@
 import axios from 'axios'
 
+const baseUrl = 'http://localhost:3000/api'
+
 export const fetchProductData = async (productId: number) => {
   const result = await axios(
-    'http://localhost:3000/api/insights/'+ productId?.toString()
+    baseUrl + '/insights/'+ productId?.toString()
   )
   return result.data
 }
 
 export const fetchProducts = async () => {
   const result = await axios(
-    'http://localhost:3000/api/products'
+    baseUrl + '/products'
   )
   return result.data
 }
